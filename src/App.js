3b28809af9b40e7da25310de43641ad58c80c16f
EngrHumza <humzaliaqat01@gmail.com>
2022-01-27 22:11:16 +0500

    Add hello.js component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './hello';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          <Hello></Hello>
         </p>
         <a
           className="App-link"
