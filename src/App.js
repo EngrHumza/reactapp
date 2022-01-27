9fe7ec7d2b9f6e155bddf1ac37b1eefd03b79b39
EngrHumza <humzaliaqat01@gmail.com>
2022-01-27 23:07:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Hello>name="Humza"</Hello>
+          <Hello name="humza"/>
         </p>
         <a
           className="App-link"
